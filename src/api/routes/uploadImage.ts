7d0c63f82080c69d5c8d22736de6b28bbecbbad3
abinashchhetri <abinashchhetri.work@gmail.com>
2024-09-02 21:32:26 +0545

    file upload done

--- src/api/routes/uploadImage.ts
+++ src/api/routes/uploadImage.ts
@@ -1,25 +1,34 @@
 import { RouterPropsType } from "../types";
 import { Router } from "express";
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+// const uploadDir = path.resolve(__dirname, "../../upload");
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir);
+// }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "upload/");
+    return cb(null, path.resolve(__dirname, "../../upload"));
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + file.originalname);
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-";
+    return cb(null, file.originalname);
   },
 });
 
-const upload = multer({ Storage: storage });
+const upload = multer({ storage });
+
 const uploadImagesRouter = Router();
+
 export default ({ router }: RouterPropsType) => {
   router.use("/upload", uploadImagesRouter);
 
   uploadImagesRouter.post(
     "/uploadImage",
-    upload.single("file"),
+    upload.single("jasu"),
     (req, res, next) => {
       try {
         res.status(200).json({ success: "file Upload sccessful" });
--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,18 @@ import express from "express";
 import config from "./config";
 import logger from "./loaders/logger";
 import "reflect-metadata";
+// import loaders from "@/loaders";
 // import Logger from "./loaders/logger";
 
 async function startServer() {
   const server = express();
-  //   loaders({ server });
+  // loaders({ server });
 
   await (await import("@/loaders")).default({ server });
 
   server.listen(config.port, () => {
     logger.info(`app started at port ${config.port}`);
   });
-
-
 }
 
 startServer();
